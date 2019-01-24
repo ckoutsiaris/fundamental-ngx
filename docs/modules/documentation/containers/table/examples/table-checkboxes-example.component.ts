fbd990d2c07a23988866374bed4ba65d12f7c80b
O'Donnell <mikerodonnell89@users.noreply.github.com>
2019-01-24 14:14:19 -0700

    table checkbox without ViewChild

--- docs/modules/documentation/containers/table/examples/table-checkboxes-example.component.ts
+++ docs/modules/documentation/containers/table/examples/table-checkboxes-example.component.ts
@@ -1,12 +1,10 @@
-import { Component, QueryList, ViewChildren } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
     selector: 'fd-table-checkboxes-example',
     templateUrl: './table-checkboxes-example.component.html'
 })
 export class TableCheckboxesExampleComponent {
-
-    @ViewChildren('localCheckbox') checkboxes: QueryList<any>;
 
     selectedRows = [];
 
@@ -18,35 +16,40 @@ export class TableCheckboxesExampleComponent {
             column2: 'Row 1',
             column3: 'Row 1',
             date: '09-07-18',
-            type: 'search'
+            type: 'search',
+            checked: false
         },
         {
             column1: 'Row 2',
             column2: 'Row 2',
             column3: 'Row 2',
             date: '09-07-18',
-            type: 'cart'
+            type: 'cart',
+            checked: false
         },
         {
             column1: 'Row 3',
             column2: 'Row 3',
             column3: 'Row 3',
             date: '09-07-18',
-            type: 'calendar'
+            type: 'calendar',
+            checked: false
         },
         {
             column1: 'Row 4',
             column2: 'Row 4',
             column3: 'Row 4',
             date: '09-07-18',
-            type: 'search'
+            type: 'search',
+            checked: false
         },
         {
             column1: 'Row 5',
             column2: 'Row 5',
             column3: 'Row 5',
             date: '09-07-18',
-            type: 'search'
+            type: 'search',
+            checked: false
         }
     ];
 
@@ -71,9 +74,9 @@ export class TableCheckboxesExampleComponent {
     }
 
     private selectAll(): void {
-        this.checkboxes.forEach((checkbox, index) => {
-            if (checkbox.nativeElement.checked === false) {
-                checkbox.nativeElement.checked = true;
+        this.tableRows.forEach((checkbox, index) => {
+            if (checkbox.checked === false) {
+                checkbox.checked = true;
                 this.selectedRows.push(index);
             }
         });
@@ -81,8 +84,8 @@ export class TableCheckboxesExampleComponent {
     }
 
     private deselectAll(): void {
-        this.checkboxes.forEach(checkbox => {
-            checkbox.nativeElement.checked = false;
+        this.tableRows.forEach(checkbox => {
+            checkbox.checked = false;
         });
         this.allSelected = false;
         this.selectedRows = [];
@@ -90,8 +93,8 @@ export class TableCheckboxesExampleComponent {
 
     private checkIfAllSelected(): boolean {
         let state = true;
-        this.checkboxes.forEach(checkbox => {
-            if (checkbox.nativeElement.checked === false) {
+        this.tableRows.forEach(checkbox => {
+            if (checkbox.checked === false) {
                 state = false;
             }
         });
